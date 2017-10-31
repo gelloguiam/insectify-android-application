fd5060a242b584600878fdbb1fd13ea2f1b5bc2c
gelloguiam <acguiam@gmail.com>
2017-10-31 18:04:42 +0800

    Add wiki to fragment
    Add local and online wiki

--- app/src/main/java/com/gelloguiam/insectrecognition/InsectWiki.java
+++ app/src/main/java/com/gelloguiam/insectrecognition/InsectWiki.java
@@ -1,13 +1,16 @@
 package com.gelloguiam.insectrecognition;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
 
 public class InsectWiki extends AppCompatActivity {
-    private static WebView wiki;
-    private static final String wiki_link = "https://en.wikipedia.org/wiki/";
+    public static String subject;
+    private static Fragment localWiki;
+    private static Fragment onlineWiki;
+    private static FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,16 +18,21 @@ public class InsectWiki extends AppCompatActivity {
         setContentView(R.layout.activity_insect_wiki);
 
         Bundle bundle = getIntent().getExtras();
-        String name = bundle.getString("name");
-        wiki = (WebView) findViewById(R.id.insect_wiki);
-        wiki.loadUrl(wiki_link + name);
-        wiki.setWebViewClient(new WebViewClient() {
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                view.loadUrl(url);
-                return true;
-            }
-        });
+        subject = bundle.getString("name");
+
+        localWiki = new LocalWikiFragment();
+        onlineWiki = new OnlineWikiFragment();
+        fragmentManager = getFragmentManager();
+
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.wiki_wrapper, localWiki);
+        fragmentTransaction.commit();
+    }
+
+    public static void openWebWiki() {
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.wiki_wrapper, onlineWiki);
+        fragmentTransaction.commit();
     }
 
     @Override
---
+++ app/src/main/java/com/gelloguiam/insectrecognition/LocalWikiFragment.java
@@ -0,0 +1,45 @@
+package com.gelloguiam.insectrecognition;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+
+public class LocalWikiFragment extends Fragment {
+
+    private ImageButton readMore;
+
+    public LocalWikiFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_local_wiki, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceType) {
+        super.onActivityCreated(savedInstanceType);
+
+        readMore = (ImageButton) getFragmentManager().
+                findFragmentById(R.id.wiki_wrapper).
+                getView().
+                findViewById(R.id.read_more);
+
+        readMore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                InsectWiki.openWebWiki();
+            }
+        });
+    }
+}
